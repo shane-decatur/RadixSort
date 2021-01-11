34bd33e1c7b8a9b67fe05446999ab84eefa88b3e
shane-decatur <sdecatur20@stuy.edu>
2021-01-11 14:36:23 -0500

    added nth()

--- RadixSort.java
+++ RadixSort.java
@@ -1,12 +1,14 @@
 import java.lang.Math;
 public class RadixSort{
   public static int nth(int n, int col){
-    return 0;
+    return (int)(n % Math.pow(10, col+1));
   }
 
   public static int length(int n){
     return 0;
   }
 
-  public static void merge(MyLinkedList original, MyLinkedList[] buckets);
+  public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+    
+  }
 }
