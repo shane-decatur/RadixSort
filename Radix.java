0b00ea9851f698a5b00316cbb69fe957ed37c173
shane-decatur <sdecatur20@stuy.edu>
2021-01-11 15:02:55 -0500

    added merge()

--- Radix.java
+++ Radix.java
@@ -10,6 +10,8 @@ public class Radix{
   }
 
   public static void merge(MyLinkedList original, MyLinkedList[] buckets){
-    
+    for (int i = 0; i < buckets.length; i++){
+      original.extend(buckets[i]);
+    }
   }
 }
