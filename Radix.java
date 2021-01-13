8e43d45460e6201b9519721de0a771f855fc3b73
shane-decatur <sdecatur20@stuy.edu>
2021-01-13 14:09:45 -0500

    idk how to fix

--- Radix.java
+++ Radix.java
@@ -14,33 +14,29 @@ public class Radix{
     }
   }
 
-  public static int maxDigit(SortableLinkedList data){
-    int max = 0;
-    for (int i = 0; i < data.size(); i++){
-      if (data.get(i) > max) max = data.get(i);
-    }
-    return length(max);
-  }
-
   public static void radixSortSimple(SortableLinkedList data){
     SortableLinkedList[] buckets = new SortableLinkedList[10];
     for (int i = 0; i < 10; i++){
       buckets[i] = new SortableLinkedList();
     }
-    for (int j = 0; j < maxDigit(data); j++){
+    int passes = 1;
+    int dataLength = data.size();
+    for (int j = 0; j < 1; j++){
       for (int i = 0; i < data.size(); i++){
-        if (nth(data.get(i), j) == 0) buckets[0].add(data.get(i));
-        if (nth(data.get(i), j) == 1) buckets[1].add(data.get(i));
-        if (nth(data.get(i), j) == 2) buckets[2].add(data.get(i));
-        if (nth(data.get(i), j) == 3) buckets[3].add(data.get(i));
-        if (nth(data.get(i), j) == 4) buckets[4].add(data.get(i));
-        if (nth(data.get(i), j) == 5) buckets[5].add(data.get(i));
-        if (nth(data.get(i), j) == 6) buckets[6].add(data.get(i));
-        if (nth(data.get(i), j) == 7) buckets[7].add(data.get(i));
-        if (nth(data.get(i), j) == 8) buckets[8].add(data.get(i));
-        if (nth(data.get(i), j) == 9) buckets[9].add(data.get(i));
-        data.remove(i);
-        i--;
+        int temp = data.get(0);
+        if (j == 0 && length(temp) > passes) passes = length(temp);
+        if (nth(temp, j) == 0) buckets[0].add(temp);
+        if (nth(temp, j) == 1) buckets[1].add(temp);
+        if (nth(temp, j) == 2) buckets[2].add(temp);
+        if (nth(temp, j) == 3) buckets[3].add(temp);
+        if (nth(temp, j) == 4) buckets[4].add(temp);
+        if (nth(temp, j) == 5) buckets[5].add(temp);
+        if (nth(temp, j) == 6) buckets[6].add(temp);
+        if (nth(temp, j) == 7) buckets[7].add(temp);
+        if (nth(temp, j) == 8) buckets[8].add(temp);
+        if (nth(temp, j) == 9) buckets[9].add(temp);
+        data.remove(0);
+        dataLength--;
       }
       merge(data,buckets);
     }
@@ -51,7 +47,9 @@ public class Radix{
     for (int i = 0; i < 19; i++){
       buckets[i] = new SortableLinkedList();
     }
-    for (int j = 0; j < maxDigit(data); j++){
+    int passes = 1;
+
+    for (int j = 0; j < passes; j++){
       for (int i = 0; i < data.size(); i++){
         if (nth(data.get(i), j) == -9) buckets[0].add(data.get(i));
         if (nth(data.get(i), j) == -8) buckets[1].add(data.get(i));
@@ -78,4 +76,5 @@ public class Radix{
       merge(data,buckets);
     }
   }
+
 }
